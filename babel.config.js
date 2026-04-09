fbf55a037e72f78e5eed4c447b6127705915cf27
Rajeev <rajeev@example.com>
2026-04-09 07:16:49 +0530

    aligned babel and entry point for sdk 54

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }] // Agar NativeWind use kar rahe hain toh
+    ],
     plugins: [
-      // Expo Router ke liye ye zaruri hai
-      'expo-router/babel',
+      // REQUIRED: Expo Router ke liye ye hona hi chahiye
     ],
   };
 };
